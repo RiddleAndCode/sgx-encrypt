5186f59fb7e8650d19ffbaecf40d447a9fa2be41
Julian Popescu <jpopesculian@gmail.com>
2021-10-30 22:06:51 +0200

    make seal key request in place

--- src/keyrequest.rs
+++ src/keyrequest.rs
@@ -1,16 +1,14 @@
 use crate::ecalls::{egetkey, ereport};
-use generic_array::{ArrayLength, GenericArray};
 use sgx_isa::{Attributes, Keyname, Keypolicy, Keyrequest};
 
 #[derive(Default, Clone, Debug, Eq, PartialEq)]
-pub struct SealKeyrequest<U: ArrayLength<u8>> {
-    pub seed: GenericArray<u8, U>,
+pub struct SealKeyrequest {
     pub keypolicy: Keypolicy,
     pub attributes: Attributes,
 }
 
-impl<U: ArrayLength<u8>> SealKeyrequest<U> {
-    pub fn key(&self) -> GenericArray<u8, U> {
+impl SealKeyrequest {
+    pub fn fill(&self, seed: &mut [u8]) {
         let report = ereport(Default::default(), [0; 64]).unwrap();
         let base_keyrequest = Keyrequest {
             keyname: Keyname::Seal as u16,
@@ -22,9 +20,7 @@ impl<U: ArrayLength<u8>> SealKeyrequest<U> {
             ..Default::default()
         };
 
-        let mut out: GenericArray<u8, U> = Default::default();
-        let mut chunks = self.seed.as_slice().chunks(16);
-        let mut offset = 0;
+        let mut chunks = seed.chunks_mut(16);
         while let Some(chunk) = chunks.next() {
             let mut keyid = [0; 32];
             keyid[..chunk.len()].copy_from_slice(chunk);
@@ -33,25 +29,30 @@ impl<U: ArrayLength<u8>> SealKeyrequest<U> {
                 ..base_keyrequest
             };
             let key = egetkey(keyrequest).unwrap();
-            out[offset..offset + chunk.len()].copy_from_slice(&key[..chunk.len()]);
-            offset += chunk.len();
+            chunk.copy_from_slice(&key[..chunk.len()]);
         }
-
-        out
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use generic_array::typenum::{U16, U32, U48, U52, U64};
+
+    fn check_size(size: usize) {
+        let req = SealKeyrequest::default();
+        let mut key = vec![0; size];
+        req.fill(&mut key);
+        assert_ne!(key, vec![0; size]);
+    }
 
     #[test]
     fn empty_seal_key_request() {
-        SealKeyrequest::<U16>::default().key();
-        SealKeyrequest::<U32>::default().key();
-        SealKeyrequest::<U48>::default().key();
-        SealKeyrequest::<U52>::default().key();
-        SealKeyrequest::<U64>::default().key();
+        check_size(16);
+        check_size(32);
+        check_size(48);
+        check_size(52);
+        check_size(64);
+        check_size(128);
+        check_size(255);
     }
 }
